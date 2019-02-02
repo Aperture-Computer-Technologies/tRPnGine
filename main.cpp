a263c21a72138e249183e974cf219b8953a2429e
PyroStefan <41029693+PyroStefan@users.noreply.github.com>
2019-02-02 15:26:34 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// C++ program to gather and respond to user information in the beginning of the game
+// C++ program to gather and respond to user information in the beginning of the game, a n-dice roller and standard responses
 
 #include <cstdlib>
 #include <iostream>
@@ -8,23 +8,32 @@ using namespace std;
 class displayInfo
 {
 public:
-  char gender, name, race, playerClass;
+  char gGender, gName, gRace, gClass;
 
   void displayAllInfo ()
   {
-    cout << "You are " << name << ", the " << gender << " " << race <<  ", " << " and a " <<
-      playerClass;
+    cout << "You are " << gName << ", the " << gGender << " " << gRace <<  ", " << " and a " <<
+      gClass;
   }
 };
 
 class dice
 {
 public:
-  int n; \\ value of dice
+  int n; // value of dice
     
-  dice(int x) \\ parametrized constructor input amount of faces the dice has
+  dice(int x) // parametrized constructor input amount of faces the dice has
   {
-    cout << "A " << x << "is rolling. . ." << endl;
-    n = rand() % x; \\ the value of the dice
+    cout << "A " << x << " dice is rolling. . ." << endl;
+    n = rand() % x; // the value of the dice
   }
 };
+
+class stdDialogue
+{
+  public:
+    char welcomeCity, battleStart, battleEnd; // standard dialogue that can be reused
+    
+    welcomeCity = "Welcome, you've arrive at ";
+    battleStart = "You entered in battle!";
+    battleEnd = "The battle has ended.";
