c20fe40ddb368586a7d391aad6b27ee284331fbd
PyroStefan <41029693+PyroStefan@users.noreply.github.com>
2019-02-01 21:17:47 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// C++ program to gather and respond to user information in the beginning of the game
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+class displayInfo
+{
+public:
+  string class, gender, name, race;
+
+  void displayAllInfo ()
+  {
+    cout << "You are " << name << ", " << gender << ", " << " and a " <<
+      class;
+  }
+};
